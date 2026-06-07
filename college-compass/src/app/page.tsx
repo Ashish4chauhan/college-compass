edb42794af78eed0c0fdda8b4f8638307eaddd3e
Ashish Chauhan <ashish04chauhan@gmail.com>
2026-06-07 05:52:39 +0530

    fix college details page

--- college-compass/src/app/page.tsx
+++ college-compass/src/app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 async function getColleges() {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_SITE_URL}/api/colleges`,
+    "http://localhost:3000/api/colleges",
     {
       cache: "no-store",
     }
